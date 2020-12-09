e91cc5ec5bd06a66d2b3c86c76a7f9deabc85991
Weblate <noreply@weblate.org>
2020-12-09 10:04:40 +0000

    Added translation using Weblate (Chinese (Pinyin))

---
+++ kylin-ipmsg/translations/kylin-ipmsg_zh_Latn.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_Latn">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="qml/dukto/AboutPage.qml" line="44"/>
+        <source>Kylin Ipmsg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/AboutPage.qml" line="66"/>
+        <source>Provide text chat and file transfer function with no server. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/AboutPage.qml" line="67"/>
+        <source>Mult person concurrency. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/AboutPage.qml" line="68"/>
+        <source>The UI resources comes from Dukto</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <location filename="chatwidget.ui" line="14"/>
+        <source>Kylin Ipmsg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="36"/>
+        <source>Send Text (Enter)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="49"/>
+        <source>Send Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="62"/>
+        <source>Send Dir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="80"/>
+        <source>Chat Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="251"/>
+        <source>Sending files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="264"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="361"/>
+        <source>Receiving files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="459"/>
+        <source>Change nickname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="chatwidget.ui" line="478"/>
+        <source>Confirm change</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IpPage</name>
+    <message>
+        <location filename="qml/dukto/IpPage.qml" line="104"/>
+        <source>Your IP Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecentPage</name>
+    <message>
+        <location filename="qml/dukto/RecentPage.qml" line="48"/>
+        <source>Sorry, there is nothing yet...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/RecentPage.qml" line="125"/>
+        <source>Recent </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="67"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="79"/>
+        <source>File Receive Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="120"/>
+        <source>Change Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="131"/>
+        <source>Theme Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="234"/>
+        <source>User Guide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/SettingsPage.qml" line="245"/>
+        <source>View Guide</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TabBar</name>
+    <message>
+        <location filename="qml/dukto/TabBar.qml" line="39"/>
+        <source>Buddy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/TabBar.qml" line="60"/>
+        <source>Recent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/TabBar.qml" line="82"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ToolBar</name>
+    <message>
+        <location filename="qml/dukto/ToolBar.qml" line="71"/>
+        <source>Received Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/ToolBar.qml" line="98"/>
+        <source>My IP Addr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/dukto/ToolBar.qml" line="123"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
